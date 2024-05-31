8f1e15a784362dd146f18a351050997001bf881a
LiangQi.Zeng <liangqi.zeng@ictrek.com>
2024-05-31 15:45:47 +0800

    [feat] enable common-lisp
    Signed-off-by: LiangQi.Zeng <liangqi.zeng@ictrek.com>

--- init.el
+++ init.el
@@ -120,7 +120,7 @@
        ;;(cc +lsp)         ; C > C++ == 1
        (clojure
         +lsp)           ; java with a lisp
-       ;;common-lisp       ; if you've seen one lisp, you've seen them all
+       common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
