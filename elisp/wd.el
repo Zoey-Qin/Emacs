4b459e11c2b67c3e42f77d88d765e2df419be60f
LiangQi.Zeng <liangqi.zeng@ictrek.com>
2024-06-13 17:36:03 +0800

    [fix/wd] use local shell
    Signed-off-by: LiangQi.Zeng <liangqi.zeng@ictrek.com>

--- elisp/wd.el
+++ elisp/wd.el
@@ -3,11 +3,11 @@
 
 (defun qi/wd-search-word ()
   (interactive)
-  (let ((shell-output
-        (shell-command-to-string
-         (concat "wd " (thing-at-point 'word 'no-properties)))))
-    (popup-tip
-     (replace-regexp-in-string "\033\\[[0-9;]*[mK]" "" shell-output))))
+  (let ((default-directory temporary-file-directory))
+    (let ((shell-output (shell-command-to-string
+                       (concat "wd " (thing-at-point 'word 'no-properties)))))
+      (popup-tip
+       (replace-regexp-in-string "\033\\[[0-9;]*[mK]" "" shell-output)))))
 
  (map! :leader
       :desc "search word by wd"          "z" #'qi/wd-search-word)
