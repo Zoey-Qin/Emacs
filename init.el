7156d67abc02bff0ca30dad15f53e9daadab6e0a
LiangQi.Zeng <liangqi.zeng@ictrek.com>
2024-06-19 15:32:30 +0800

    [package] enable cc
    Signed-off-by: LiangQi.Zeng <liangqi.zeng@ictrek.com>

--- init.el
+++ init.el
@@ -117,7 +117,8 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       ;;(cc +lsp)         ; C > C++ == 1
+       (cc +lsp
+           +tree-sitter)         ; C > C++ == 1
        (clojure
         +lsp)           ; java with a lisp
        common-lisp       ; if you've seen one lisp, you've seen them all
